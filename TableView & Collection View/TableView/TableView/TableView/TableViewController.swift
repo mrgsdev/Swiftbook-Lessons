b844bd4a4cd735b690c9ae8765d3b84e99a088aa
mrgsdev <mrgsdev@gmail.com>
2022-08-27 13:13:49 +0300

    add cell Swipe action

--- TableView & Collection View/TableView/TableView/TableView/TableViewController.swift
+++ TableView & Collection View/TableView/TableView/TableView/TableViewController.swift
@@ -40,6 +40,9 @@ class TableViewController: UITableViewController {
         return cell
     }
     
+    
+    
+    
     override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
         return .delete
     }
@@ -61,5 +64,34 @@ class TableViewController: UITableViewController {
         objects.insert(movedEmoji, at: destinationIndexPath.row)
         tableView.reloadData()
         
+    }
+    
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let done = doneAction(at: indexPath)
+        let favourite = favouriteAction(at: indexPath)
+        return UISwipeActionsConfiguration(actions: [done,favourite])
+    }
+    
+    func doneAction(at indexPath:IndexPath) -> UIContextualAction {
+        let action = UIContextualAction(style: .destructive, title: "Done") { action, view, completion in
+            self.objects.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            completion(true)
+        }
+        action.backgroundColor = .systemGreen
+        action.image = UIImage(systemName: "checkmark.circle")
+        return action
+    }
+    
+    func favouriteAction(at indexPath:IndexPath) -> UIContextualAction {
+        var object = objects[indexPath.row]
+        let action = UIContextualAction(style: .normal, title: "Favourite") { action, view, completion in
+            object.isFavourite = !object.isFavourite
+            self.objects[indexPath.row] = object
+            completion(true)
+        }
+        action.backgroundColor = object.isFavourite ? .systemPurple : .systemGray
+        action.image = object.isFavourite ? UIImage(systemName: "heart.slash") : UIImage(systemName: "heart")
+        return action
     }
 }
