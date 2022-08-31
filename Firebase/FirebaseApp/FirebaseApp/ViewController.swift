7cec17c55be4787cbd903e03f17e68a205099de8
mrgsdev <mrgsdev@gmail.com>
2022-08-31 13:34:00 +0300

    add

---
+++ Firebase/FirebaseApp/FirebaseApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseApp
+//
+//  Created by MRGS on 31.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
