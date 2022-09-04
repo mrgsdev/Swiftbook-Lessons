efaa25020426e717bc25b61583aff3550c3d0c74
mrgsdev <mrgsdev@gmail.com>
2022-09-04 14:11:56 +0300

    add UI

---
+++ Firebase/FirebaseApp/FirebaseApp/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  FirebaseApp
+//
+//  Created by MRGS on 31.08.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var warnLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad() 
+        
+    }
+    
+  
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+        
+    }
+
+}
+
---
+++ Firebase/FirebaseApp/FirebaseApp/TasksViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TasksViewController.swift
+//  FirebaseApp
+//
+//  Created by MRGS on 04.09.2022.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var tableview: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableview.delegate = self
+        tableview.dataSource = self
+    }
+      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = "This is cell number \(indexPath.row)"
+        cell.textLabel?.textColor = .white
+        return cell
+    }
+    
+      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    @IBAction func addTapped(_ sender: UIBarButtonItem) {
+        
+    }
+   
+}
