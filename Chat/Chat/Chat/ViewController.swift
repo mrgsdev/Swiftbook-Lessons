b134a52ccb7c4b4c2055ef2ac83fd25ec0804c24
mrgsdev <mrgsdev@gmail.com>
2022-10-25 14:31:08 +0300

    add Chat

---
+++ Chat/Chat/Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by MRGS on 25.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
