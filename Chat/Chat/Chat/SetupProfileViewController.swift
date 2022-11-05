80dee7771d5b2fe0823438daf422242c2d209389
mrgsdev <mrgsdev@gmail.com>
2022-11-05 13:41:18 +0300

    PhotoVC

---
+++ Chat/Chat/Chat/SetupProfileViewController.swift
@@ -0,0 +1,58 @@
+//
+//  SetupProfileViewController.swift
+//  Chat
+//
+//  Created by MRGS on 07.12.2022.
+//
+
+
+import UIKit
+
+class SetupProfileViewController: UIViewController {
+    
+    let fillImageView = AddPhotoView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        setupConstraints()
+    }
+}
+
+// MARK: - Setup constraints
+extension SetupProfileViewController {
+    private func setupConstraints() {
+        
+        fillImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(fillImageView)
+        
+        NSLayoutConstraint.activate([
+            fillImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 160),
+            fillImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+}
+
+// MARK: - SwiftUI
+import SwiftUI
+
+struct SetupProfileVCProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let setupProfileVC = SetupProfileViewController()
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<SetupProfileVCProvider.ContainerView>) -> SetupProfileViewController {
+            return setupProfileVC
+        }
+        
+        func updateUIViewController(_ uiViewController: SetupProfileVCProvider.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<SetupProfileVCProvider.ContainerView>) {
+            
+        }
+    }
+}
+
